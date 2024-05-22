7df2b58c81291430b2c75c0281053073f8b0a669
Shimaa Elnaggar <shimaaibrahim072@gmail.com>
2024-05-22 16:00:33 +0300

    create container around fields

--- lib/views/sign_up_view.dart
+++ lib/views/sign_up_view.dart
@@ -1,10 +1,8 @@
-
-
 import 'package:ectd_task9/widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 
 class SignUpView extends StatelessWidget {
-    SignUpView({super.key});
+  SignUpView({super.key});
 
   var emailController = TextEditingController();
 
@@ -20,49 +18,53 @@ class SignUpView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepOrange,
-      body: ListView(
-        children: [
-          CustomTextFormField(
-            obscureText: false,
-            maxLength: 25,
-            textInputAction: TextInputAction.next,
-            keyboardType: TextInputType.emailAddress,
-            label: const Text("Email"),
-            controller: emailController,
-          ),
-          CustomTextFormField(
-            obscureText: false,
-            maxLength: 10,
-            textInputAction: TextInputAction.next,
-            keyboardType: TextInputType.name,
-            label: const Text("Username"),
-            controller: userNameController,
-          ),
-          CustomTextFormField(
-              obscureText: true,
+      body: Container(
+        margin: const EdgeInsets.symmetric(vertical: 60,horizontal: 30),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: ListView(
+          children: [
+            CustomTextFormField(
+              obscureText: false,
+              maxLength: 25,
+              textInputAction: TextInputAction.next,
+              keyboardType: TextInputType.emailAddress,
+              label: const Text("Email"),
+              controller: emailController,
+            ),
+            CustomTextFormField(
+              obscureText: false,
               maxLength: 10,
               textInputAction: TextInputAction.next,
+              keyboardType: TextInputType.name,
+              label: const Text("Username"),
+              controller: userNameController,
+            ),
+            CustomTextFormField(
+                obscureText: true,
+                maxLength: 10,
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.text,
+                label: const Text("Password"),
+                controller: passwordController,
+                suffixIcon: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.visibility_off,
+                  ),
+                )),
+            CustomTextFormField(
+              obscureText: true,
+              maxLength: 10,
+              textInputAction: TextInputAction.done,
               keyboardType: TextInputType.text,
-              label: const Text("Password"),
-              controller: passwordController,
-              suffixIcon: IconButton(
-                onPressed: () {
-
-                },
-                icon:Icon(
-                  Icons.visibility_off,
-                ) ,
-              )
-          ),
-          CustomTextFormField(
-            obscureText: true,
-            maxLength: 10,
-            textInputAction: TextInputAction.done,
-            keyboardType: TextInputType.text,
-            label: const Text("Confirm Password"),
-            controller: confirmPasswordController,
-          ),
-        ],
+              label: const Text("Confirm Password"),
+              controller: confirmPasswordController,
+            ),
+          ],
+        ),
       ),
     );
   }
