bece012499c7012c40311786a212c7be18c5bfcf
Shimaa Elnaggar <shimaaibrahim072@gmail.com>
2024-05-22 15:38:07 +0300

    Create Sign_up_view

--- lib/views/sign_up_view.dart
+++ lib/views/sign_up_view.dart
@@ -1,11 +1,29 @@
 
+
 import 'package:flutter/material.dart';
 
 class SignUpView extends StatelessWidget {
-  const SignUpView({super.key});
+    SignUpView({super.key});
+
+  var emailController = TextEditingController();
+
+  var userNameController = TextEditingController();
+
+  var passwordController = TextEditingController();
+
+  var confirmPasswordController = TextEditingController();
+
+  var formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.deepOrange,
+      body: ListView(
+        children: [
+
+        ],
+      ),
+    );
   }
 }
