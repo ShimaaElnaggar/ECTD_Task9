1ab7fed3fe08ceac6a8c05d298b4fd354b6785a7
Shimaa Elnaggar <shimaaibrahim072@gmail.com>
2024-05-22 19:33:57 +0300

    colors edition

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:ectd_task9/views/sign_up_view.dart';
 import 'package:flutter/material.dart';
 
@@ -11,10 +12,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-
       home: SignUpView(),
     );
   }
